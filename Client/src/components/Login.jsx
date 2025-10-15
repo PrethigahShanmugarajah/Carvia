bac0b5279fdd981473ce58209ab204e1e03a0de0
PrethigahShanmugarajah <prethigah@gmail.com>
2025-10-16 01:16:04 +0530

    Completed Login component and finished frontend section of Carvia project

--- Client/src/components/Login.jsx
+++ Client/src/components/Login.jsx
@@ -1,9 +1,136 @@
-import React from "react";
+import React, { useState } from "react";
+import { Eye, EyeOff } from "lucide-react";
 
-const Login = () => {
+const Login = ({ setShowLogin }) => {
+  const [state, setState] = useState("login");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+
+  const onSubmitHandler = async (event) => {
+    event.preventDefault();
+  };
+
   return (
-    <div>
-      <h1>Login</h1>
+    <div
+      onClick={() => setShowLogin(false)}
+      className="fixed top-0 bottom-0 left-0 right-0 z-100 flex items-center text-sm text-gray-600 bg-black/50"
+    >
+      <form
+        onSubmit={onSubmitHandler}
+        onClick={(e) => e.stopPropagation()}
+        className="flex flex-col gap-4 m-auto items-start p-8 py-12 w-80 sm:w-[352px] text-black rounded-lg shadow-xl border border-gray-200 bg-white"
+      >
+        <p className="text-2xl font-medium m-auto">
+          <span className="text-primary">User</span>{" "}
+          {state === "login" ? "Login" : "Sign Up"}
+        </p>
+        {state === "register" && (
+          <div className="w-full">
+            <p>Name</p>
+            <input
+              onChange={(e) => setName(e.target.value)}
+              value={name}
+              placeholder="Name"
+              className="border border-gray-200 rounded w-full p-2 mt-1 outline-primary"
+              type="text"
+              required
+            />
+          </div>
+        )}
+
+        <div className="w-full ">
+          <p>Email</p>
+          <input
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+            placeholder="Email"
+            className="border border-gray-200 rounded w-full p-2 mt-1 outline-primary"
+            type="email"
+            required
+          />
+        </div>
+
+        <div className="w-full relative">
+          <p>Password</p>
+          <input
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+            placeholder="Password"
+            className="border border-gray-200 rounded w-full p-2 mt-1 outline-primary"
+            type={showPassword ? "text" : "password"}
+            required
+          />
+          <span
+            className="absolute top-[34px] right-2 cursor-pointer"
+            onClick={() => setShowPassword(!showPassword)}
+          >
+            {showPassword ? (
+              <EyeOff className="w-5 h-5 text-primary" />
+            ) : (
+              <Eye className="w-5 h-5 text-primary-dull" />
+            )}
+          </span>
+        </div>
+
+        {state === "register" && (
+          <div className="w-full relative">
+            <p>Confirm Password</p>
+            <input
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              value={confirmPassword}
+              placeholder="Confirm Password"
+              className="border border-gray-200 rounded w-full p-2 mt-1 outline-primary"
+              type={showConfirmPassword ? "text" : "password"}
+              required
+            />
+            <span
+              className="absolute top-[34px] right-2 cursor-pointer"
+              onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+            >
+              {showConfirmPassword ? (
+                <EyeOff className="w-5 h-5 text-primary" />
+              ) : (
+                <Eye className="w-5 h-5 text-primary-dull" />
+              )}
+            </span>
+          </div>
+        )}
+
+        {state === "register" &&
+          password !== confirmPassword &&
+          confirmPassword.length > 0 && (
+            <p className="text-red-500 text-xs">Passwords do not match</p>
+          )}
+
+        {state === "register" ? (
+          <p>
+            Already have account?{" "}
+            <span
+              onClick={() => setState("login")}
+              className="text-primary cursor-pointer"
+            >
+              click here
+            </span>
+          </p>
+        ) : (
+          <p>
+            Create an account?{" "}
+            <span
+              onClick={() => setState("register")}
+              className="text-primary cursor-pointer"
+            >
+              click here
+            </span>
+          </p>
+        )}
+        <button className="bg-primary hover:bg-primary-dull transition-all text-white w-full py-2 rounded-md cursor-pointer">
+          {state === "register" ? "Create Account" : "Login"}
+        </button>
+      </form>
     </div>
   );
 };
